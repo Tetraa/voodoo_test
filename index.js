6e7ed4cf089814d0df1321c58294693209beb616
Giudici <giudici@MBP-Giudici.home>
2021-11-12 17:52:23 +0100

    feature b: populate the database with the top 100

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const db = require('./models');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
+const gplay = require('google-play-scraper');
+const store = require('app-store-scraper');
 
 const app = express();
 
@@ -59,7 +61,7 @@ app.post('/api/games/search', (req, res) => {
   if (name) {
     whereClose.name = { [Op.like]: `%${name}%` };
   }
-  if(platform === 'ios' || platform === 'android') whereClose.platform = platform;
+  if (platform === 'ios' || platform === 'android') whereClose.platform = platform;
   return db.Game.findAll({
     where: whereClose
   })
@@ -69,6 +71,79 @@ app.post('/api/games/search', (req, res) => {
       return res.status(400).send(err);
     });
 });
+
+
+app.get('/api/games/populate', (req, res) => {
+  requestAndroidTopGames(res, populateAfterProcess);
+});
+
+function populateAfterProcess(androidGames, iosGames, res) {
+  const gameList = [];
+
+  for (const game in androidGames) {
+    const item = androidGames[game];
+    gameList.push({ 
+      publisherId: item.developerId, 
+      name: item.title, 
+      platform: 'android', 
+      storeId: item.appId, 
+      bundleId: item.appId, 
+      isPublished: true 
+    });
+  }
+
+  for (const game in iosGames) {
+    const item = iosGames[game];
+    gameList.push({ 
+      publisherId: item.developerId.replace('?uo=2', ''),
+      name: item.title, 
+      platform: 'ios', 
+      storeId: item.id,
+      bundleId: item.appId, 
+      isPublished: true 
+    });
+  }
+
+  return populateDBWithTop100(gameList, res);
+}
+
+function populateDBWithTop100(gameList, res) {
+  return db.Game.bulkCreate(gameList, {
+    fields: ['publisherId', 'name', 'platform', 'storeId', 'bundleId', 'isPublished'],
+    ignoreDuplicates: true
+  })
+  .then(games => res.send(games))
+  .catch((err) => {
+    console.log('***There was an error creating the top 1000 games of each store', JSON.stringify(err));
+    return res.status(400).send(err);
+  });
+}
+
+function requestAndroidTopGames(res, callback) {
+  gplay.list({
+    collection: gplay.collection.TOP_FREE,
+    num: 100
+  }).then(games => {
+    requestIosTopGames(games, res, callback)
+  }).catch((err) => {
+    console.log('***There was an error querying top android games', JSON.stringify(err));
+    return res.status(400).send(err);
+  });
+}
+
+function requestIosTopGames(androidGames, res, callback) {
+  store.list({
+    collection: store.collection.TOP_FREE,
+    num: 100
+  })
+    .then((iosGames) => {
+      callback(androidGames, iosGames, res);
+    })
+    .catch((err) => {
+      console.log('***There was an error querying top ios games', JSON.stringify(err));
+      return res.status(400).send(err);
+    });
+}
 
 
 function findAll(res) {
